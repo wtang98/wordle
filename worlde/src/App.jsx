ffa2e4f6c4bcfecb9a7e3aa7800cc9059b2a2c17
Wei-Tong Tang <weitongblue@gmail.com>
2022-03-30 19:05:28 +0100

    created parrallel game board to color the grid

--- worlde/src/App.jsx
+++ worlde/src/App.jsx
@@ -1,17 +1,20 @@
 import './App.scss';
 import {useState, useEffect, createContext} from 'react'
 import Home from './components/home';
-import { startingBoard } from './startingboard'
+import { startingBoard } from './jses/startingColorBoard'
+import { startingColorBoard } from './jses/colorBoard';
 
 export const ContextAPI = createContext();
 
 function App() {
 
     const [board, setBoard] = useState(startingBoard);
+    const [colorBoard, setColorBoard] = useState(startingColorBoard)
     const [currentAttempt, setCurrentAttempt] = useState({attempt:0, letterPos:0})
     const [allWords, setAllWords] = useState('')
     const [fiveLetterWords, setfiveLetterWords] = useState('')
     const [randomFiveLetter, setRandomFiveLetter] = useState('')
+
     const [grey, setGrey] = useState(false)
     const [yellow, setYellow] = useState(false)
     const [green, setGreen] = useState(false)
@@ -37,11 +40,11 @@ function App() {
     if(randomFiveLetter!= undefined){
         ansArr = randomFiveLetter.split('')
     }
-    // console.log(fiveLetterWords)
+    console.log(randomFiveLetter)
 
     return (
         <div className="App">
-            <ContextAPI.Provider value={{randomFiveLetter, fiveLetterWords, board, setBoard, currentAttempt, setCurrentAttempt, grey, setGrey, yellow, setYellow, green, setGreen}}>
+            <ContextAPI.Provider value={{randomFiveLetter, fiveLetterWords, board, setBoard, colorBoard, setColorBoard, currentAttempt, setCurrentAttempt, grey, setGrey, yellow, setYellow, green, setGreen}}>
                 <Home/>
             </ContextAPI.Provider>
         </div>
--- worlde/src/components/gameArea/row/letter/letter.jsx
+++ worlde/src/components/gameArea/row/letter/letter.jsx
@@ -4,12 +4,14 @@ import './letter.scss'
 
 const Letter = ({letterI, attemptI}) => {
 
-    const {board} = useContext(ContextAPI)
+    const {board, colorBoard} = useContext(ContextAPI)
 
     const letter = board[attemptI][letterI]
+    const color = colorBoard[attemptI][letterI]
 
+    // console.log(green, 'is correct?')
     return (
-        <div className='letter'>   
+        <div className={`letter ${color}`}>   
             {letter}
             {/* <input type="text" defaultValue={letter} value = {letter} maxLength={1} /> */}
         </div>
